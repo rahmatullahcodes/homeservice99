396b6e31dbf76c48178057575513ca36770f96d4
Md Rahmatullah Quraishi <113741633+rahmatullahcodes@users.noreply.github.com>
2025-12-26 15:15:03 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -114,7 +114,7 @@ export default function Navbar() {
         </div>
 
         {/* MOBILE HAMBURGER (visible on small screens) */}
-        <button 
+        {/* <button 
           className="mobile-hamburger" 
           onClick={() => setMobileMenu(m => !m)} 
           aria-expanded={mobileMenu} 
@@ -123,7 +123,7 @@ export default function Navbar() {
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M3 6H21M3 12H21M3 18H21" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
           </svg>
-        </button>
+        </button> */}
 
       </div>
 
